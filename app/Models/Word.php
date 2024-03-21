594225d126dc31aa26a33482d11cc9ab2cd630c1
mariocstweb <mariocstmra@gmail.com>
2024-03-21 16:50:57 +0100

    added softdeletes and fillable

--- app/Models/Word.php
+++ app/Models/Word.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Word extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $fillable = ['title', 'description', 'slug',];
 }
