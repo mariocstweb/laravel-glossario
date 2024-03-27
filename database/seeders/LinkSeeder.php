714c73ca18e47544d81fa3706aa13dc214835c11
mariocstweb <mariocstmra@gmail.com>
2024-03-27 12:15:03 +0100

    filled link seeder

--- database/seeders/LinkSeeder.php
+++ database/seeders/LinkSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Link;
+use App\Models\Word;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +15,27 @@ class LinkSeeder extends Seeder
     public function run(): void
     {
         //
+        $words_ids = Word::pluck('id')->toArray();
+        $links = [
+            ['title' => 'Wikipedia', 'url' => 'https://it.wikipedia.org/wikiPagina_principale'],
+            ['title' => 'W3schools', 'url' => 'https://www.w3schools.com/'],
+            ['title' => 'Treccani', 'url' => 'https://www.treccani.it/vocabolario/'],
+            ['title' => 'Bootstrap', 'url' => 'https://getbootstrap.com/'],
+            ['title' => 'Freecodecamp', 'url' => 'https://www.freecodecamp.org/italian/news'],
+            ['title' => 'MDN', 'url' => 'https://developer.mozilla.org/en-US/'],
+            ['title' => 'Googlefonts', 'url' => 'https://fonts.google.com/'],
+        ];
+
+        foreach ($links as $link) {
+            $new_link = new Link();
+
+            $new_link->title = $link['title'];
+            $new_link->url = $link['url'];
+
+            $new_link->save();
+
+            $link_words = array_filter($words_ids, fn () => rand(0, 1));
+            $new_link->words()->attach($link_words);
+        }
     }
 }
