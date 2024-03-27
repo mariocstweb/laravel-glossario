1a5982d0a0f3beb46ed10d35dd6346e9121e0bd0
mariocstweb <mariocstmra@gmail.com>
2024-03-27 12:23:07 +0100

    fixed

---
+++ database/migrations/2024_03_27_112002_add_word_id_to_links_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Word;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('links', function (Blueprint $table) {
+            //
+            $table->foreignIdFor(Word::class)->after('id')->nullable()->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->dropForeignIdFor(Word::class);
+            //
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Link;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(LinkSeeder::class);
     }
 }
