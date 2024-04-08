dfc3406b65566f6805ec713232049d3d20fa3fc6
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-04-08 14:39:33 +0200

    added button into home

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,7 +4,8 @@
 <div class="jumbotron p-5 my-5 bg-light rounded-3 shadow ">
     <div class="container py-5">
         <h1 class="display-5 fw-bold text-center">
-            BENVENUTO SUL TUO GLOSSARIO
+            BENVENUTO SUL TUO GLOSSARIO <br>
+            <a href="{{route('admin.words.index') }}" class="btn btn-primary">Vai al Glossario</a>
         </h1>
     </div>
 </div>
